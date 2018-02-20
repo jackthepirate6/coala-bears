7093fc564df51c40e7b907c90c89bfb38c7fc7c0
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2018-02-20 10:59:49 +0530

    Update DocGrammarBear.py

--- bears/documentation/DocGrammarBear.py
+++ bears/documentation/DocGrammarBear.py
@@ -96,21 +96,28 @@ class DocGrammarBear(DocBaseClass, LocalBear):
             List of rules to disable checks for.
         """
         for doc_comment in self.extract(file, language, docstyle):
-            parsed = doc_comment.parse()
-
-            (new_metadata, warning_desc) = self.process_documentation(
-                parsed, locale, languagetool_disable_rules)
-
-            new_comment = DocumentationComment.from_metadata(
-                new_metadata, doc_comment.docstyle_definition,
-                doc_comment.marker, doc_comment.indent, doc_comment.position)
-
-            if new_comment != doc_comment:
-                # Something changed, let's apply a result.
-                diff = self.generate_diff(file, doc_comment, new_comment)
-
-                yield Result(
+            if isinstance(doc_comment, MalformedComment):
+                yield Result.from_values(
                     origin=self,
-                    message=warning_desc,
-                    affected_code=(diff.range(filename),),
-                    diffs={filename: diff})
+                    message=doc_comment.message,
+                    file=filename,
+                    line=doc_comment.line + 1)
+            else:    
+                parsed = doc_comment.parse()
+
+                (new_metadata, warning_desc) = self.process_documentation(
+                    parsed, locale, languagetool_disable_rules)
+
+                new_comment = DocumentationComment.from_metadata(
+                    new_metadata, doc_comment.docstyle_definition,
+                    doc_comment.marker, doc_comment.indent, doc_comment.position)
+
+                if new_comment != doc_comment:
+                    # Something changed, let's apply a result.
+                    diff = self.generate_diff(file, doc_comment, new_comment)
+
+                    yield Result(
+                        origin=self,
+                        message=warning_desc,
+                        affected_code=(diff.range(filename),),
+                        diffs={filename: diff})
